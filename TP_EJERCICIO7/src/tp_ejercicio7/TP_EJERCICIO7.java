06ea82d814d709f6c9ae4d1f0df926be57c21422
matiasandrada333et20-jpg <matiasandrada333.et20@gmail.com>
2026-05-29 21:53:47 -0300

    s

--- TP_EJERCICIO7/src/tp_ejercicio7/TP_EJERCICIO7.java
+++ TP_EJERCICIO7/src/tp_ejercicio7/TP_EJERCICIO7.java
@@ -1,20 +1,54 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package tp_ejercicio7;
+import java.util.Scanner;
 
-/**
- *
- * @author shefa
- */
 public class TP_EJERCICIO7 {
 
-    /**
-     * @param args the command line arguments
-     */
+ 
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner lector = new Scanner(System.in);
+
+        // Ingreso de dimensiones de la matriz
+        System.out.print("Ingrese el número de filas: ");
+        int filas = lector.nextInt();
+
+        System.out.print("Ingrese el número de columnas: ");
+        int columnas = lector.nextInt();
+
+        int[][] matriz = new int[filas][columnas];
+
+      
+        System.out.println("Ingrese los elementos de la matriz:");
+
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                System.out.print("Elemento [" + i + "][" + j + "]: ");
+                matriz[i][j] = lector.nextInt();
+            }
+        }
+
+        int suma = 0;
+        int contador = 0;
+
+        // Recorre la matriz
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+
+              
+                if ((i > 0 || j > 0) && ((i + j) % 2 != 0)) {
+                    suma += matriz[i][j];
+                    contador++;
+                }
+            }
+        }
+
+        // Calcula el promedio
+        if (contador > 0) {
+            double promedio = (double) suma / contador;
+            System.out.println("El promedio es: " + promedio);
+        } else {
+            System.out.println("No existen elementos en posiciones positivas impares.");
+        }
+        
     }
-    
 }
+    
