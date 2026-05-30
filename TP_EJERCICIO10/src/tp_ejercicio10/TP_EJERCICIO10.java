425bae5df0779000c06e363cc835dafe0d36bfdb
matiasandrada333et20-jpg <matiasandrada333.et20@gmail.com>
2026-05-29 22:47:59 -0300

    SUBO NUEVO PROYECTO

---
+++ TP_EJERCICIO10/src/tp_ejercicio10/TP_EJERCICIO10.java
@@ -0,0 +1,93 @@
+package tp_ejercicio10;
+import java.util.Random;
+
+public class TP_EJERCICIO10 {
+
+   
+   static int[] BuscarExtremos(int[][] Matriz){
+        int[] Extremos = new int[4];
+
+        //pos 0 = valor mas alto
+        //pos 1 = dia del valor mas alto
+        //pos 2 = valor mas bajo
+        Extremos[2] = 40;
+        //pos 3 = dia del valor mas bajo
+
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j < 7; j++){
+
+                if(Extremos[0] < Matriz[i][j]){
+                    Extremos[0] = Matriz[i][j];
+                    Extremos[1] = j;
+                }
+
+                if(Extremos[2] > Matriz[i][j]){
+                    Extremos[2] = Matriz[i][j];
+                    Extremos[3] = j;
+                }
+            }
+        }
+        return Extremos;
+    }
+
+    static float BuscarPromedio(int[][] Matriz, int semana){
+        float promedio = 0.0f;
+        int contador = 0;
+
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j < 7; j++){
+                promedio = promedio + Matriz[i][j];
+                contador++;
+
+                if(contador >= 31) break;
+            }
+        }
+
+        promedio = promedio / 31.0f;
+        return promedio;
+    }
+
+    public static void main(String[] args) {
+
+        int[][] temperaturas = new int[5][7];
+
+        //funcion llenar matriz
+        temperaturas = LlenarMatriz(temperaturas, 7, 38);
+
+        //funcion buscar promedio
+        float promedio = BuscarPromedio(temperaturas, 0);
+        System.out.println("el promedio de la semana 0 es " + promedio);
+
+        int[] Resultado = BuscarExtremos(temperaturas);
+
+        System.out.println("temperatura mas alta = " + Resultado[0]);
+        System.out.println("dia de T mas alta = " + Resultado[1]);
+        System.out.println("temperatura mas baja = " + Resultado[2]);
+        System.out.println("dia de T mas baja = " + Resultado[3]);
+
+        //mostrar resultados
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j < 7; j++){
+                System.out.print(temperaturas[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static int[][] LlenarMatriz(int[][] Matriz, int min, int max){
+        Random Aleatorio = new Random();
+        int contador = 0;
+
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j < 7; j++){
+                Matriz[i][j] = Aleatorio.nextInt(min, max);
+                contador = contador + 1;
+                contador++;
+
+                if(contador >= 31) break;
+            }
+        }
+
+        return Matriz;
+    }
+}
